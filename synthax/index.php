8eda7e3fa93c982fdc79607477a1473d3dcd6bb7
zbahati <bahatizirimwabagabo@gmail.com>
2024-02-14 13:20:18 +0200

    synthax for how to write php code case sensitive and non sensitive

---
+++ synthax/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>synthax</title>
+
+</head>
+<body>
+  <?php
+$text = "Hello world" .
+    '<br/>';
+echo $text;
+
+//  php if , else , while , function, classes are not case_sensitive;
+// example
+
+echo $text;
+EcHO $text;
+?>
+</body>
+</html>
