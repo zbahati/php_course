7e5a963859046d30e65e27f764eafdfe5b5f64b3
zbahati <bahatizirimwabagabo@gmail.com>
2024-02-14 15:04:08 +0200

    working with constant like const keyword and define key word that reserve two perams one is name and other is value const

---
+++ phpconstant/index.php
@@ -0,0 +1,14 @@
+<?php
+
+// Constant in php is imutabe means can not change and is accessible intirely in script
+// case_sensitive
+define("DB", "mysql");
+
+echo DB;
+
+
+// case_insensitive, is Deprecated in php 8 so it will leave warning mean that we can remove the sensitive boolean type to excep deff errors
+
+
+define("Name", "Bahati");
+echo Name;
