2ac5c6e78adfa483ad02c06642d5de4d3fb33eac
zbahati <bahatizirimwabagabo@gmail.com>
2024-02-14 14:56:41 +0200

    working with maths function in php

---
+++ phpmath/index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo(pi());
+echo "<br/>";
+echo "MIN AND MAX";
+echo(min(10, 30, 40, 50, 3, 4));
+echo "<br/>";
+echo(max(10, 30, 40, 50, 3, 4));
+
+echo "random number ";
+echo (rand());
+echo "<br/>";
+echo (rand(10, 13));
+echo "<br/>";
+
+echo "Round the number : ";
+echo(round(0.5));
+echo "<br/>";
+echo(round(0.49));
